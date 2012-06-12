dbc8b702cb863d375565085fd9313bf9aa10632b
Pirdad Sakhizada <pirdad@bnotions.com>
2012-06-12 01:35:33 -0400

    the layout changed to table. much easier to work with.

--- application/views/v_income.php
+++ application/views/v_income.php
@@ -1,3 +1,5 @@
+
+<!-- PHP Variable Declarations -->
 <?php 
 	$list_income = array(
 		array('title'=>'Job','date'=>'14-April-2012', 'amount'=>toCurrency(1400.25), 'note'=>'Work at so and so place'),
@@ -15,43 +17,62 @@
 	
 ?>
 
-
+<!-- PHP Variable Usage -- The variables above are used here for displaying them in html -->
 <div>
 	<p>Income Sources for <? echo $month.' - '.$year; ?></p>
 </div>
 <div>
 	<?
-	// displays income sources row by row using the for loop...
+	
+	// displays income sources row by row using the for-loop...
 	for ($i = 0; $i < count($list_income); $i++) 
 	{
-		echo '<div class="item">';
-		
-		$item_pic = '<img class="item_pic" width="60px" height="60px" />';
-		$item_title =  '<div class="item_title trebuchet_font_title">'.$list_income[$i]['title'].'</div>';
-		$item_date = '<div class="item_date trebuchet_font_date">'.$list_income[$i]['date'].'</div>';
-		$item_amount = '<div class="item_amount trebuchet_font_default">$'.$list_income[$i]['amount'].'</div>';
-		$item_v_line = '<div class="item_vertical_line"></div>';
-		$item_edit = '<img class="img_edit" width="30px" height="30px" src="media/icons/edit.png" />';
-		$item_delete = '<img class="img_delete" width="30px" height="30px" src="media/icons/delete.png" />';
-		$item_note = '<div class="item_note trebuchet_font_note">'.$list_income[$i]['note'].'</div>';
-		
-		echo $item_pic;
-		echo '<div class="item_text_container" >';
-		echo $item_title.$item_date.$item_amount.$item_note;
-		echo '</div>';
-		echo '<div class="item_options_container" >';
-		echo $item_edit.$item_delete;
-		echo '</div>';
-		
-		echo '</div>';
-		echo $item_v_line;
-	}	
 	
-	
-	echo '<div id="container_total" >';
-	echo '<div class="item_vertical_line"></div>';
-	echo '<div id="title_total" >Total Income: </div>';
-	echo '<div id="items_total" >'.$total_income.'</div>';
-	echo '</div>';
 	?>
+		<div class="item">
+			
+			<table width="100%" cellspacing="0px" cellpadding="0px">
+				<tr>
+					<td rowspan="2" width="60px">
+						<img width="60px" height="60px" src="" class="item_picture"/>
+					</td>
+					<td >
+						<span class="item_title trebuchet_font-family"> <? echo $list_income[$i]['title']; ?> </span> <span>-</span>
+						<span class="item_date trebuchet_font-family"> <? echo $list_income[$i]['date']; ?> </span>
+					</td>
+					<td rowspan="2" width="150px" align="right">
+						<span class="item_amount trebuchet_font-family"> <? echo $list_income[$i]['amount']; ?> </span>
+					</td>
+					<td rowspan="2" width="10px">
+						<div class="item_horizontal_seperator"></div>
+					</td>
+					<td rowspan="2" width="80px" align="right">
+						<img width="30px" height="30px" src="media/icons/edit.png" />
+						<img width="30px" height="30px" src="media/icons/delete.png" />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<span class="item_note trebuchet_font-family"> <? echo $list_income[$i]['note']; ?> </span>
+					</td>
+				</tr>
+			</table>
+			<div class="item_vertical_line"></div>
+		</div>
+	<?	
+	} // end of for-loop
+	?>
+	
+	<div id="container_total" >
+	
+		<div class="item_vertical_line"></div>
+		<div id="title_total" >
+			Total Income: 
+		</div>
+		<div id="items_total" >
+			<? echo $total_income; ?>
+		</div>
+	
+	</div>
+	
 </div>
